5235eaf26616ee6ededdede911483de6a708e939
Anthony Nowell <anthony@algorithmia.com>
2015-04-17 23:26:52 -0700

    Doc update

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['algorithmia'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='algorithmia/struct.Service.html' title='algorithmia::Service'>Service</a>",];
+implementors['algorithmia'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='algorithmia/struct.Service.html' title='algorithmia::Service'>Service</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['algorithmia'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='algorithmia/algorithm/enum.Version.html' title='algorithmia::algorithm::Version'>Version</a>&lt;'a&gt;",];
+implementors['algorithmia'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='algorithmia/algorithm/enum.Version.html' title='algorithmia::algorithm::Version'>Version</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
