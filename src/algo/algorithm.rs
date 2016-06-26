872a1445fb8e683d5913ad1c364e7b988a5ad2cb
Anthony Nowell <anthony@algorithmia.com>
2016-06-25 23:05:44 -0600

    Fix broken doc test

--- src/algo/algorithm.rs
+++ src/algo/algorithm.rs
@@ -325,8 +325,9 @@ impl Algorithm {
     /// # use algorithmia::Algorithmia;
     /// # use algorithmia::algo::Algorithm;
     /// let client = Algorithmia::client("111112222233333444445555566");
-    /// let minmax  = client.algo("codeb34v3r/FindMinMax/0.1").timeout(3);
-    /// minmax.pipe(vec![2,3,4]);
+    /// client.algo("codeb34v3r/FindMinMax/0.1")
+    ///     .timeout(3)
+    ///     .pipe(vec![2,3,4]);
     /// ```
     pub fn timeout(&mut self, timeout: u32) -> &mut Algorithm {
         self.options.timeout(timeout);
