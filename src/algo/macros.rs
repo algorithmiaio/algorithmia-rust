043289bac384e609d965432469351a1e9e847f54
Anthony Nowell <anthony@algorithmia.com>
2016-11-13 21:09:12 -0700

    Document the algo_entrypoint macro

--- src/algo/macros.rs
+++ src/algo/macros.rs
@@ -1,3 +1,163 @@
+/// Macro for implementing `Entrypoint` or `DecodedEntryPoint` boilerplate in an algorithm
+///
+/// Algorithmia support for Rust algorithms is hard-coded to call `Algo::apply(AlgoInput)`
+///   but for the sake of convenience the `EntryPoint` trait provides a default implementation
+///   of `apply` that delegates to `apply_str`, `apply_bytes`, or `apply_json` depending on
+///   the variant of `AlgoInput`. The `DecodedEntryPoint` trait overrides the default `apply`
+///   to provide a method that uses a deserialized associated type when calling `apply_decoded`.
+///   This pattern is incredibly flexible, but adds boilerplate and still requires
+///   converting output and error types back.
+///
+/// The `algo_entrypoint!` macro hides all that boilerplate code behind a single macro invocation.
+///   `algo_entrypoint(input => your_fn)` wires up the boilerplate for calling `your_fn`
+///    according to specified input where these values are accepted:
+///
+/// - `text`: calls `your_fn(&str)`
+/// - `bytes`: calls `your_fn(&[u8])`
+/// - `JsonValue`: calls `your_fn(&JsonValue)`
+/// - `AlgoInput`: calls `your_fn(AlgoInput)`
+/// - `T`: calls `your_fn(T)` (T must be deserializable)
+///
+/// In all cases, the return value of `your_fn` should be `Result<T, E>` where
+///   `T` impl `Into<AlgoOutput>` which includes `String`, `Vec<u8>`, `JsonValue`, and boxed serializeable types
+///   `E` impl `Into<Box<Error>>` which includes `String` and all `Error` types
+///
+/// # Examples
+///
+/// ## Text Input/Output
+/// To set the entrypoint to a function that receives and returns text:
+///
+/// ```no_run
+/// # #[macro_use] extern crate algorithmia;
+/// # fn main() {}
+/// # use algorithmia::prelude::*;
+/// algo_entrypoint!(text => hello_text);
+///
+/// fn hello_text(input: &str) -> Result<String, String> {
+///     unimplemented!()
+/// }
+/// ```
+///
+/// which generates:
+///
+/// ```ignore
+/// #[derive(Default)]
+/// pub struct Algo;
+///
+/// impl EntryPoint for Algo {
+///     fn apply_str(&self, input: &str) -> Result<AlgoOutput, Box<::std::error::Error>> {
+///         hello_text(input).map(AlgoOutput::from).map_err(|err| err.into())
+///     }
+/// }
+///
+/// fn hello_text(input: &str) -> Result<String, String> {
+///     unimplemented!()
+/// }
+/// ```
+///
+/// ## Binary Input/Output
+/// To set the entrypoint to a function that receives and returns binary data:
+///
+/// ```no_run
+/// # #[macro_use] extern crate algorithmia;
+/// # fn main() {}
+/// # use algorithmia::prelude::*;
+/// algo_entrypoint!(bytes => hello_bytes);
+///
+/// fn hello_bytes(input: &[u8]) -> Result<Vec<u8>, String> {
+///     unimplemented!()
+/// }
+/// ```
+///
+/// ## JSON Input/Output
+/// To set the entrypoint to a function that receives and returns JSON data,
+///   you can work directly with the `JsonValue`:
+///
+/// ```no_run
+/// # #[macro_use] extern crate algorithmia;
+/// # fn main() {}
+/// # use algorithmia::prelude::*;
+/// algo_entrypoint!(JsonValue => hello_json);
+///
+/// fn hello_json(input: &JsonValue) -> Result<JsonValue, String> {
+///     unimplemented!()
+/// }
+/// ```
+///
+/// Although, often the preferred way to work with JSON is to automatically
+///   deserialize into and serialize from a custom type.
+///
+/// ```ignore
+/// # #[macro_use] extern crate algorithmia;
+/// # fn main() {}
+/// # use algorithmia::prelude::*;
+/// #[derive(RustcDecodable)]
+/// pub struct MyInput { corpus: String, msg: String }
+///
+/// #[derive(RustcEncodable)]
+/// pub struct MyOutput { probabilities: Vec<(String, f32)> }
+///
+/// algo_entrypoint!(MyInput => hello_custom);
+///
+/// fn hello_custom(input: MyInput) -> Result<Box<MyOutput>, String> {
+///     unimplemented!()
+/// }
+/// ```
+///
+/// Note: `Box` for serializeable output is currently required for lack of specialization.
+///   The specialization implementation already exists behind the `nightly` feature.
+///   Alternatively, you can return `AlgoOutput` via `AlgoOutput::from(&myOutput)`
+///
+/// The previous example expands into:
+///
+/// ```ignore
+/// #[derive(RustcDecodable)]
+/// pub struct MyInput { corpus: String, msg: String };
+///
+/// #[derive(RustcEncodable)]
+/// pub struct MyOutput { probabilities: Vec<(String, f32)> };
+///
+/// #[derive(Default)]
+/// pub struct Algo;
+///
+/// impl DecodedEntryPoint for Algo {
+///     type Input (String, String);
+///     fn apply_decoded(&self, input: MyInput) -> Result<AlgoOutput, Box<::std::error::Error>> {
+///         hello_custom(input).map(AlgoOutput::from).map_err(|err| err.into())
+///     }
+/// }
+///
+/// fn hello_custom(input: MyInput) -> Result<Box<MyOutput>, String> {
+///     unimplemented!()
+/// }
+/// ```
+///
+/// ## Customizing `Default`
+/// And finally, it is possible to provide a custom `Default` implementation
+///   and pass that state into your method by prefixing `your_fn` with
+///   the `Algo::` namespace.
+///
+/// ```no_run
+/// # #[macro_use] extern crate algorithmia;
+/// # fn main() {}
+/// # use algorithmia::prelude::*;
+/// pub struct Algo{ init_id: String }
+///
+/// algo_entrypoint!(text => Algo::hello_text);
+///
+/// impl Algo {
+///     fn hello_text(&self, input: &str) -> Result<String, String> {
+///         unimplemented!()
+///     }
+/// }
+///
+/// impl Default for Algo {
+///     fn default() -> Algo {
+///         Algo { init_id: "foo".into() }
+///     }
+/// }
+/// ```
+
 
 
 #[macro_export]
